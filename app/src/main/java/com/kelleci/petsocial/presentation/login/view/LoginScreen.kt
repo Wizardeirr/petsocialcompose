84fe677ee033841c07bed8d3761c28ba8b2ce772
volkankelleci <volkan.kelleci@mobileitm.com>
2025-04-07 12:24:36 +0300

    ADDED, LoginScreen new Login option (google)

--- app/src/main/java/com/kelleci/petsocial/presentation/login/view/LoginScreen.kt
+++ app/src/main/java/com/kelleci/petsocial/presentation/login/view/LoginScreen.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -20,24 +23,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.kelleci.petsocial.R
 import com.kelleci.petsocial.Screen
-import com.kelleci.petsocial.presentation.tooling.DevicePreviews
 import com.kelleci.petsocial.util.Constants
 
 @Composable
 fun LoginScreen(
     navController: NavController,
-    value: String,
-    onChange: (String) -> Unit,
     submit: () -> Unit,
-    modifier: Modifier = Modifier,
-    label: String = "Password",
-    placeholder: String = "Enter your Password"
+    modifier: Modifier = Modifier
 
 ) {
     var userNameValue by remember { mutableStateOf("") }
@@ -116,6 +115,17 @@ fun LoginScreen(
             navController.navigate(Screen.RegisterScreen.route)
         }) {
             Text("Kayıt Ol")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(modifier = Modifier.fillMaxWidth(), onClick = {
+
+
+        }) {
+            Icon(painter = painterResource(R.drawable.google), "", modifier = Modifier.size(24.dp))
+            Spacer(modifier = Modifier.width(10.dp))
+            Text("Google İle Giriş Yap")
+
         }
     }
 
