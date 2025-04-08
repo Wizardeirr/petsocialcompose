620e0a43ff7cf29b4078b961a15a60275d1974a5
volkankelleci <volkan.kelleci@mobileitm.com>
2025-04-08 10:54:34 +0300

    ADDED, fill max size and register function

--- app/src/main/java/com/kelleci/petsocial/presentation/register/RegisterScreen.kt
+++ app/src/main/java/com/kelleci/petsocial/presentation/register/RegisterScreen.kt
@@ -1,19 +1,19 @@
 package com.kelleci.petsocial.presentation.register
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -30,7 +30,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kelleci.petsocial.data.UserRegister
-import com.kelleci.petsocial.presentation.tooling.DevicePreviews
 import java.util.UUID
 
 @Preview
@@ -48,177 +47,205 @@ fun RegisterScreen(
     var selectedPet by remember { mutableStateOf(RegisterConstant.petGenius[0]) }
     var selectedPetSpay by remember { mutableStateOf(RegisterConstant.petSpay[0]) }
 
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = CenterHorizontally,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(1.dp)
-        ) {
-            TextField(
-                value = userID,
-                onValueChange = { newValue ->
-                    userID = newValue
-                },
-                modifier = Modifier,
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Done,
-                    keyboardType = KeyboardType.Password
-                ),
-                keyboardActions = KeyboardActions(),
-                label = { Text("Kullanıcı Adı") },
-                singleLine = true,
-                shape = RoundedCornerShape(9.dp),
-                maxLines = 1,
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = CenterHorizontally,
+        modifier = Modifier
+            .padding(1.dp)
+            .fillMaxSize()
+    ) {
+        TextField(
+            value = userID,
+            onValueChange = { newValue ->
+                userID = newValue
+            },
+            modifier = Modifier,
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done,
+                keyboardType = KeyboardType.Password
+            ),
+            keyboardActions = KeyboardActions(),
+            label = { Text("Kullanıcı Adı") },
+            singleLine = true,
+            shape = RoundedCornerShape(9.dp),
+            maxLines = 1,
 
-                )
-            Spacer(modifier = Modifier.height(16.dp))
-            TextField(
-                value = userPetName,
-                onValueChange = { newValue ->
-                    userPetName = newValue
-                },
-                modifier = Modifier,
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Done,
-                    keyboardType = KeyboardType.Password
-                ),
-                keyboardActions = KeyboardActions(),
-                label = { Text("Evcil Hayvanızın Adı") },
-
-                singleLine = true,
-                shape = RoundedCornerShape(9.dp),
-                maxLines = 1,
-
-                )
-            Spacer(modifier = Modifier.height(16.dp))
-            RegisterConstant.petGenius.forEach { text ->
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Modifier
-                        .fillMaxWidth()
-                        .selectable(
-                            selected = (text == selectedPet),
-                            onClick = { selectedPet = text })
-                        .padding(10.dp)
-                }
-                Text(
-                    text = text,
-                    fontSize = 16.sp,
-                    modifier = Modifier.padding(start = 10.dp)
-                )
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-            TextField(
-                value = userPetGenius,
-                onValueChange = { newValue ->
-                    userPetGenius = newValue
-                },
-                modifier = Modifier,
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Done,
-                    keyboardType = KeyboardType.Password
-                ),
-                keyboardActions = KeyboardActions(),
-                label = { Text("Evcil Hayvanınızın Cinsi") },
-                singleLine = true,
-                shape = RoundedCornerShape(9.dp),
-                maxLines = 1,
-
-                )
-            Spacer(modifier = Modifier.height(16.dp))
-            TextField(
-                value = userPetKg,
-                onValueChange = { newValue ->
-                    userPetKg = newValue
-                },
-                modifier = Modifier,
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Done,
-                    keyboardType = KeyboardType.Password
-                ),
-                keyboardActions = KeyboardActions(),
-                label = { Text("Kilosu") },
-
-                singleLine = true,
-                shape = RoundedCornerShape(9.dp),
-                maxLines = 1,
-
-                )
-            Spacer(modifier = Modifier.height(16.dp))
-            TextField(
-                value = userPetAge,
-                onValueChange = { newValue ->
-                    userPetAge = newValue
-                },
-                modifier = Modifier,
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Done,
-                    keyboardType = KeyboardType.Password
-                ),
-                keyboardActions = KeyboardActions(),
-                label = { Text("Yaşı") },
-
-                singleLine = true,
-                shape = RoundedCornerShape(9.dp),
-                maxLines = 1,
-
-                )
-            Spacer(modifier = Modifier.height(16.dp))
-            TextField(
-                value = userPetVaccine,
-                onValueChange = { newValue ->
-                    userPetVaccine = newValue
-                },
-                modifier = Modifier,
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Done,
-                    keyboardType = KeyboardType.Password
-                ),
-                keyboardActions = KeyboardActions(),
-                label = { Text("Aşı Durumu") },
-                singleLine = true,
-                shape = RoundedCornerShape(9.dp),
-                maxLines = 1,
-
-                )
-            Spacer(modifier = Modifier.height(16.dp))
-
-            RegisterConstant.petSpay.forEach { text ->
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Modifier
-                        .fillMaxWidth()
-                        .selectable(
-                            selected = (text == selectedPetSpay),
-                            onClick = { selectedPetSpay = text })
-                        .padding(10.dp)
-                }
-                Text(
-                    text = text,
-                    fontSize = 16.sp,
-                    modifier = Modifier.padding(start = 10.dp)
-                )
-            }
-        }
-        if (userID.isNotEmpty() &&
-            userPetName.isNotEmpty() &&
-            userPetGenius.isNotEmpty() &&
-            userPetKg.isNotEmpty() &&
-            userPetAge.isNotEmpty() &&
-            userPetVaccine.isNotEmpty()
-        ) {
-            val userRegister = UserRegister(
-                userID = userID,
-                userPetSpecies = userPetSpecies,
-                userPetGenius = userPetGenius,
-                userPetKg = userPetKg,
-                userPetAge = userPetAge,
-                userPetVaccine = userPetVaccine,
-                userPetSpay = userPetSpay,
-                userPetName = userPetName,
-                userUUID = UUID.randomUUID()
             )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = userPetName,
+            onValueChange = { newValue ->
+                userPetName = newValue
+            },
+            modifier = Modifier,
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done,
+                keyboardType = KeyboardType.Password
+            ),
+            keyboardActions = KeyboardActions(),
+            label = { Text("Evcil Hayvanızın Adı") },
 
-            //viewModel.saveRegisterInformation(userRegister)
+            singleLine = true,
+            shape = RoundedCornerShape(9.dp),
+            maxLines = 1,
+
+            )
+        Spacer(modifier = Modifier.height(16.dp))
+        RegisterConstant.petGenius.forEach { text ->
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Modifier
+                    .fillMaxWidth()
+                    .selectable(
+                        selected = (text == selectedPet),
+                        onClick = { selectedPet = text })
+                    .padding(10.dp)
+            }
+            Text(
+                text = text,
+                fontSize = 16.sp,
+                modifier = Modifier.padding(start = 10.dp)
+            )
         }
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = userPetGenius,
+            onValueChange = { newValue ->
+                userPetGenius = newValue
+            },
+            modifier = Modifier,
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done,
+                keyboardType = KeyboardType.Password
+            ),
+            keyboardActions = KeyboardActions(),
+            label = { Text("Evcil Hayvanınızın Cinsi") },
+            singleLine = true,
+            shape = RoundedCornerShape(9.dp),
+            maxLines = 1,
 
+            )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = userPetKg,
+            onValueChange = { newValue ->
+                userPetKg = newValue
+            },
+            modifier = Modifier,
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done,
+                keyboardType = KeyboardType.Password
+            ),
+            keyboardActions = KeyboardActions(),
+            label = { Text("Kilosu") },
+
+            singleLine = true,
+            shape = RoundedCornerShape(9.dp),
+            maxLines = 1,
+
+            )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = userPetAge,
+            onValueChange = { newValue ->
+                userPetAge = newValue
+            },
+            modifier = Modifier,
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done,
+                keyboardType = KeyboardType.Password
+            ),
+            keyboardActions = KeyboardActions(),
+            label = { Text("Yaşı") },
+
+            singleLine = true,
+            shape = RoundedCornerShape(9.dp),
+            maxLines = 1,
+
+            )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = userPetVaccine,
+            onValueChange = { newValue ->
+                userPetVaccine = newValue
+            },
+            modifier = Modifier,
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done,
+                keyboardType = KeyboardType.Password
+            ),
+            keyboardActions = KeyboardActions(),
+            label = { Text("Aşı Durumu") },
+            singleLine = true,
+            shape = RoundedCornerShape(9.dp),
+            maxLines = 1,
+
+            )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        RegisterConstant.petSpay.forEach { text ->
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Modifier
+                    .fillMaxWidth()
+                    .selectable(
+                        selected = (text == selectedPetSpay),
+                        onClick = { selectedPetSpay = text })
+                    .padding(10.dp)
+            }
+            Text(
+                text = text,
+                fontSize = 16.sp,
+                modifier = Modifier.padding(start = 10.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(modifier = Modifier.fillMaxWidth(), onClick = {
+            saveUserRegisterInfo(
+                userID,
+                userPetName,
+                userPetGenius,
+                userPetKg,
+                userPetAge,
+                userPetVaccine,
+                userPetSpecies,
+                userPetSpay
+            )
+        }) {
+            Spacer(modifier = Modifier.width(10.dp))
+            Text("Devam")
+        }
+    }
+
+
+}
+
+fun saveUserRegisterInfo(
+    userID: String, userPetName: String, userPetGenius: String, userPetKg: String,
+    userPetAge: String,
+    userPetVaccine: String,
+    userPetSpecies: Boolean,
+    userPetSpay: Boolean
+) {
+    if (userID.isNotEmpty() &&
+        userPetName.isNotEmpty() &&
+        userPetGenius.isNotEmpty() &&
+        userPetKg.isNotEmpty() &&
+        userPetAge.isNotEmpty() &&
+        userPetVaccine.isNotEmpty()
+    ) {
+        val userRegister = UserRegister(
+            userID = userID,
+            userPetSpecies = userPetSpecies,
+            userPetGenius = userPetGenius,
+            userPetKg = userPetKg,
+            userPetAge = userPetAge,
+            userPetVaccine = userPetVaccine,
+            userPetSpay = userPetSpay,
+            userPetName = userPetName,
+            userUUID = UUID.randomUUID()
+        )
+
+        //viewModel.saveRegisterInformation(userRegister)
+    }else{
+
+    }
 }
