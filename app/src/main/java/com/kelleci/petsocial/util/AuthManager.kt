8caf9a5d3afc4a083074a388c165e9786df874e5
volkankelleci <kellecivolkan@gmail.com>
2025-04-17 09:30:47 +0300

    ADDED, Telephony auth added

--- app/src/main/java/com/kelleci/petsocial/util/AuthManager.kt
+++ app/src/main/java/com/kelleci/petsocial/util/AuthManager.kt
@@ -4,14 +4,10 @@ import android.app.Activity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.PhoneAuthOptions
 import com.google.firebase.auth.PhoneAuthProvider
-import com.google.firebase.auth.PhoneAuthProvider.OnVerificationStateChangedCallbacks
 import java.util.concurrent.TimeUnit
 
 object AuthManager {
-    fun googleAuth(){
-
-    }
-    fun telephoneNumberAuth(activity: Activity, phoneNumber: String, auth:FirebaseAuth,callbacks: OnVerificationStateChangedCallbacks){
+    fun telephoneNumberAuth(activity: Activity, phoneNumber: String, auth: FirebaseAuth, callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks){
         val options = PhoneAuthOptions.newBuilder(auth)
             .setPhoneNumber(phoneNumber) // Phone number to verify
             .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
