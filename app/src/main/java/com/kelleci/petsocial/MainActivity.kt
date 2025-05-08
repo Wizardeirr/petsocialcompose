3baceffa96187134d9c91aef2c2130a5f158eb4b
volkan kelleci <kellecivolkan@gmail.com>
2025-05-08 11:08:03 +0300

    ADDED, Android Entry Point

--- app/src/main/java/com/kelleci/petsocial/MainActivity.kt
+++ app/src/main/java/com/kelleci/petsocial/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -13,7 +14,9 @@ import com.kelleci.petsocial.presentation.login.telephonelogin.TelephoneLogin
 import com.kelleci.petsocial.presentation.login.view.LoginScreen
 import com.kelleci.petsocial.presentation.register.RegisterScreen
 import com.kelleci.petsocial.ui.theme.PetsocialTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
